1c74c438b3431fb9f690f091c021ed02a310d56c
Sarthak Teotia <sarthak.clevdoc@gmail.com>
2024-08-07 15:14:38 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
-})
+  base: '/', // Make sure this matches your deployment base path
+});
