fc8d2b280a2dd3ee60e98934f15a2cfb66149301
Sarthak Teotia <sarthak.clevdoc@gmail.com>
2024-08-07 15:18:53 +0530

    ss

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config();
+
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  optimizeDeps: {
+    include: ['sunflower-antd'], // Explicitly include sunflower-antd if not auto-detected
+  },
 })
